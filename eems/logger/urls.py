172444dd715c00a2fad4761f473d1d6a862c4f33
Anton <dzanto@gmail.com>
2020-11-13 16:45:51 +0300

    started

--- eems/logger/urls.py
+++ eems/logger/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'logger'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:claim_id>/', views.detail, name='detail')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.ClaimDetailView.as_view(), name='claim_detail'),
+    path('address/<int:pk>/', views.AddressDetailView.as_view(), name='address_detail'),
 ]
--- eems/logger/views.py
+++ eems/logger/views.py
@@ -1,19 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-from .models import Claim
+from .models import Claim, Address
+from django.views import generic
+from django.urls import reverse
 
 
-def index(request):
-    latest_claims = Claim.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_claims': latest_claims,
-    }
-    return render(request, 'logger/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'logger/index.html'
+    context_object_name = 'latest_claims'
 
-def detail(request, claim_id):
-    try:
-        claim = Claim.objects.get(pk=claim_id)
-    except Claim.DoesNotExist:
-        raise Http404('Заявка с данным ID не найдена')
-    return render(request, 'logger/detail.html', {'claim': claim})
+    def get_queryset(self):
+        return Claim.objects.order_by('-pub_date')[:5]
+
+
+class ClaimDetailView(generic.DetailView):
+    model = Claim
+    template_name = 'logger/claim_detail.html'
+
+
+class AddressDetailView(generic.DetailView):
+    model = Address
+    template_name = 'logger/address_detail.html'
+
+
+# def index(request):
+#     latest_claims = Claim.objects.order_by('-pub_date')[:5]
+#     addresses = Address.objects.all()
+#     context = {
+#         'latest_claims': latest_claims,
+#         'addresses': addresses,
+#     }
+#     return render(request, 'logger/index.html', context)
+#
+#
+# def detail(request, claim_id):
+#     claim = get_object_or_404(Claim, pk=claim_id)
+#     return render(request, 'logger/claim_detail.html', {'claim': claim})
+#
+#
+# def address_detail(request, address_id):
+#     address = get_object_or_404(Address, pk=address_id)
+#     return render(request, 'logger/address_detail.html', {'address': address})
 
