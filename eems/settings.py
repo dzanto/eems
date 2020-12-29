33577afc92ed11c8cda846fc4d191e10a9383944
Anton <dzanto@gmail.com>
2020-12-29 10:15:20 +0300

    ditignore

--- eems/settings.py
+++ eems/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DATETIME_INPUT_FORMATS += ('%Y-%m-%dT%H:%M:%S',)
 
